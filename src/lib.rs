660d4635777d85824edd80897eb1f1b54b6a3190
h0mtanks <gurnoorsingh3@gmail.com>
2022-12-14 22:08:09 +0530

    pair combinator

--- src/lib.rs
+++ src/lib.rs
@@ -27,6 +27,44 @@ pub fn match_literal(expected: &'static str) -> impl Fn(&str) -> Result<(&str, (
     // }
 }
 
+pub fn identifier(input: &str) -> Result<(&str, String), &str> {
+    let mut matched = String::new();
+    let mut chars = input.chars();
+
+    match chars.next() {
+        Some(next) if next.is_alphabetic() => matched.push(next),
+        _ => return Err(input),
+    }
+
+    while let Some(next) = chars.next() {
+        if next.is_alphanumeric() || next == '-' {
+            matched.push(next);
+        } else {
+            break;
+        }
+    }
+
+    let next_index = matched.len();
+    Ok((&input[next_index..], matched))
+}
+
+pub fn pair<P1, P2, R1, R2>(
+    parser1: P1,
+    parser2: P2,
+) -> impl Fn(&str) -> Result<(&str, (R1, R2)), &str>
+where
+    P1: Fn(&str) -> Result<(&str, R1), &str>,
+    P2: Fn(&str) -> Result<(&str, R2), &str>,
+{
+    move |input| match parser1(input) {
+        Ok((next_input, result1)) => match parser2(next_input) {
+            Ok((final_input, result2)) => Ok((final_input, (result1, result2))),
+            Err(err) => Err(err),
+        },
+        Err(err) => Err(err),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -40,5 +78,32 @@ mod tests {
             parse_joe("Hello Joe! Hello Robert!")
         );
         assert_eq!(Err("Hello Mike!"), parse_joe("Hello Mike!"));
+    }
+
+    #[test]
+    fn identifier_parser() {
+        assert_eq!(
+            Ok(("", "i-am-an-identifier".to_string())),
+            identifier("i-am-an-identifier")
+        );
+        assert_eq!(
+            Ok((" entirely an identifier", "not".to_string())),
+            identifier("not entirely an identifier")
+        );
+        assert_eq!(
+            Err("!not at all an identifier"),
+            identifier("!not at all an identifier")
+        );
+    }
+
+    #[test]
+    fn pair_combinator() {
+        let tag_opener = pair(match_literal("<"), identifier);
+        assert_eq!(
+            Ok(("/>", ((), "my-first-element".to_string()))),
+            tag_opener("<my-first-element/>")
+        );
+        assert_eq!(Err("oops"), tag_opener("oops"));
+        assert_eq!(Err("!oops"), tag_opener("<!oops"));
     }
 }
